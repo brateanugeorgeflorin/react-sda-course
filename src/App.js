a5ecccf5012ba916f191b6b0a0210846b5921e3d
Florin Brateanu <florin.brateanu@gendigital.com>
2023-11-09 18:59:43 +0200

    Routes setup

--- src/App.js
+++ src/App.js
@@ -2,19 +2,23 @@ import { AboutState, IncrementOption, OpositeButton } from './AboutState';
 import { HeaderComponent } from './components/HeaderComponent';
 import { Home } from './components/Home';
 import { UseEffect } from './components/UseEffect';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 
 function App() {
   
   return (
     <div className="App container">
-      
-        <HeaderComponent/>
-        <AboutState />
-        <IncrementOption />
-        <OpositeButton />
-        <Home />
-        <UseEffect />
-        
+        <Router>
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/aboutstate' element={<AboutState />} />
+            <Route path='/useeffect' element={<UseEffect />} />
+            <Route path='/opositebutton' element={<OpositeButton />} />
+            <Route path='/incrementoption' element={<IncrementOption />} />
+            <Route path='/headercomponent' element={<HeaderComponent />} />
+            <Route path='*' element={<p>Page does not exists.</p>} />
+          </Routes>
+        </Router>  
     </div>
   );
 }
--- src/components/Home.js
+++ src/components/Home.js
@@ -5,7 +5,6 @@ export const Home = () => {
 
     return(
         <div className="App mt-5 pb-5 row row-cols-3">
-            <div className="col-12 pb-2"><hr/></div>
             <div className="heading h1 col-12 text-center pb-2">User component</div>
             {Users.map((user, i) => {
                return <User name={user.name} age={user.age} key={user.key} id={user.key} />;
